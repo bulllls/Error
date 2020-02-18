6028250a81a0dae05fb21ceeea721a2d40b16462
Ruslan Filistovich <ruslan.filistovich@intexsoft.by>
2020-02-18 13:30:36 +0300

    Initial Commit

---
+++ Error/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Error
+//
+//  Created by Ruslan Filistovich on 18/02/2020.
+//  Copyright © 2020 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
